039023fc69a913e69a7a7c7dda3fa2b04614feec
Rajat Kumar <rajatsrajesh@gmail.com>
2026-05-12 23:10:42 +0530

    day 10 qus 2

--- src/main/java/in/rajat/day_10/MaxMin.java
+++ src/main/java/in/rajat/day_10/MaxMin.java
@@ -1,7 +1,9 @@
 package in.rajat.day_10;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 
 //Find max and min in one go using summarizingInt
@@ -18,6 +20,18 @@ public class MaxMin {
 
         System.out.println(max.getAsInt());//50
         System.out.println(min.getAsInt());//10
+
+        IntSummaryStatistics result = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.summarizingInt(Integer::intValue));//Collectors.summarizingInt() is a shortcut
+                                                                       //collector in Java 8 Streams that calculates
+                                                                       //multiple statistics in one pass.like
+                                                                       //1.count all passed value
+                                                                       //2.sum of all 3.min 4.avg 5.max
+                                                                       //summarizingInt need ToIntFunction pass object to int
+        System.out.println(result.getMax());//50
+        System.out.println(result.getMin());//10
+        System.out.println(result);//IntSummaryStatistics{count=5, sum=150, min=10, average=30.000000, max=50}
 
     }
 }
