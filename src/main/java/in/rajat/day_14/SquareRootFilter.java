64e2d0ab0c83e8f23c038656a77510902a4ba150
Rajat Kumar <rajatsrajesh@gmail.com>
2026-05-21 22:15:00 +0530

    day 14 qus 1

---
+++ src/main/java/in/rajat/day_14/SquareRootFilter.java
@@ -0,0 +1,17 @@
+package in.rajat.day_14;
+
+import java.util.List;
+
+public class SquareRootFilter {
+
+    public static void main(String[] aa) {
+        List<Integer> numbers = List.of(4, 9, 16, 25, 7);
+
+        List<Integer> result = numbers.stream()
+                .filter(num -> Math.sqrt(num)%1 ==0)
+                .toList();
+
+        System.out.println(result);
+
+    }
+}
