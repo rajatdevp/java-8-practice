68844522d7424bdbb960633c4562c2b4e67b2baf
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-01 22:37:30 +0530

    level 2 day 7 problem 1

--- src/main/java/in/rajat/level_2/day_6/MaxProduct.java
+++ src/main/java/in/rajat/level_2/day_6/MaxProduct.java
@@ -1,4 +1,34 @@
 package in.rajat.level_2.day_6;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class MaxProduct {
+
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(10,20,5,30,15);
+
+        int max = nums.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(2)
+                .reduce(1, (x, y) -> x * y);//wrong contains
+
+
+        int maxProduct = nums.stream()
+                .flatMap(num1 -> nums.stream().map(num2 -> num1 * num2))
+                .max(Integer::compareTo)
+                .orElse(0);//wrong contains duplicates multiple
+
+      Optional<Integer> result  = nums.stream()
+                .flatMap(num1 -> nums
+                        .stream()
+                        .filter(num2 -> !num2.equals(num1))
+                        .map(num2 -> num1*num2)).max(Integer::compareTo);//correct
+
+      System.out.println(result.orElse(0));
+      //System.out.println(maxProduct);
+
+
+    }
 }
