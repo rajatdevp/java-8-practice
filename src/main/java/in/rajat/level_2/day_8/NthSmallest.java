54637de2127cab6a1264ef5f9677da7cbc1e8af1
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-04 21:33:09 +0530

    level 2 day 7 problem 2

---
+++ src/main/java/in/rajat/level_2/day_8/NthSmallest.java
@@ -0,0 +1,23 @@
+package in.rajat.level_2.day_8;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class NthSmallest {
+
+    public static void main(String[] args){
+
+        List<Integer> nums = List.of(50,10);
+        List<Integer> nums2 = List.of(30,20);
+
+       Optional<Integer> result =  Stream.of(nums, nums2)
+                .flatMap(List::stream)
+                .sorted()
+                .skip(2)
+                .findFirst();
+
+       System.out.println(result.orElse(0));
+
+    }
+}
