268296f7d6d2a7c4bf0d860faeb2bb7e50ea63e6
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-04 21:27:56 +0530

    level 2 day 7 problem 1

--- src/main/java/in/rajat/level_2/day_8/RemoveDuplicateAndsSumEven.java
+++ src/main/java/in/rajat/level_2/day_8/RemoveDuplicateAndsSumEven.java
@@ -1,6 +1,7 @@
 package in.rajat.level_2.day_8;
 
 import java.util.List;
+import java.util.Optional;
 
 public class RemoveDuplicateAndsSumEven {
 
@@ -8,11 +9,10 @@ public class RemoveDuplicateAndsSumEven {
 
         List<Integer> nums = List.of(2,4,2,6,8,4);
 
-       List<Integer> result =  nums.stream()
+       Optional<Integer>result =  nums.stream()
                .distinct()
-               .filter(num -> num % 2 == 0)
-               .toList();
+               .filter(num -> num % 2 == 0).reduce(Integer::sum);
 
-       System.out.println(result);
+       System.out.println(result.orElse(0));
     }
 }
