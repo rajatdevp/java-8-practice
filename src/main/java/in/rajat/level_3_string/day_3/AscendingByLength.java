e9e93c0638d3a392845ba50ff4f3dd8a1390efdb
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-08 19:47:25 +0530

    level 3 day 3 problem 3

---
+++ src/main/java/in/rajat/level_3_string/day_3/AscendingByLength.java
@@ -0,0 +1,15 @@
+package in.rajat.level_3_string.day_3;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class AscendingByLength {
+    public static void main(String[] args){
+
+        List<String> list = List.of("a","abc","ab","abcd");
+        List<String> result = list.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+        System.out.println(result);
+    }
+}
