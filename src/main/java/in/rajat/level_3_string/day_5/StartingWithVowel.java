cd5fdb4fb71ea655e2c37a8b12998420907abca3
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-11 22:20:49 +0530

    level 3 day 1 problem 3

---
+++ src/main/java/in/rajat/level_3_string/day_5/StartingWithVowel.java
@@ -0,0 +1,17 @@
+package in.rajat.level_3_string.day_5;
+
+import java.util.List;
+
+
+
+public class StartingWithVowel {
+
+    public static void main(String[] args){
+        List<String> words = List.of("apple","banana","egg","orange");
+
+        List<String> result = words.stream()
+                .filter(word -> "aeiou".indexOf(word.charAt(0))>=0)
+                .toList();
+        System.out.println(result);
+    }
+}
