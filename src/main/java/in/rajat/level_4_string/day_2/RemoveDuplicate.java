c0cabcf3e77f6f7906a00395ef8acd2206c94eee
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-14 03:54:00 +0530

    level 4 day 1 problem 1

---
+++ src/main/java/in/rajat/level_4_string/day_2/RemoveDuplicate.java
@@ -0,0 +1,20 @@
+package in.rajat.level_4_string.day_2;
+
+import java.util.List;
+
+/*
+Remove duplicates from list of strings (preserve order)
+ */
+public class RemoveDuplicate {
+
+    public static void main(String[] args){
+
+        List<String> words = List.of("apple", "banana", "apple", "cherry", "banana");
+
+        List<String> distinctResult = words.stream()
+                .distinct()
+                .toList();
+
+        System.out.println(distinctResult);
+    }
+}
