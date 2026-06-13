534f1bed6c8b163aef233875a30abcf7b4a65ae3
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-13 21:59:29 +0530

    level 3 day 7 problem 3

---
+++ src/main/java/in/rajat/level_4_string/day_1/ContainsDigits.java
@@ -0,0 +1,24 @@
+package in.rajat.level_4_string.day_1;
+
+import java.util.List;
+
+public class ContainsDigits {
+
+    public static void main(String[] args) {
+
+       List<String> words = List.of("java8", "code2025", "stream", "java17");
+
+       List<String> regXResult =  words.stream()
+               .filter(word -> word.matches(".*\\d+.*"))
+               .toList();
+
+       System.out.println(regXResult);
+
+       List<String> streamResult =  words.stream()
+               .filter(word -> word.chars().anyMatch(Character::isDigit))
+               .toList();
+
+       System.out.println(streamResult);
+
+    }
+}
