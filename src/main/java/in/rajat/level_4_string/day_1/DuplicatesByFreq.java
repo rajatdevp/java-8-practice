d249eeb91b3bcca060bd2bba4a7f7738b68159ec
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-14 03:49:22 +0530

    level 4 day 1 problem 3-resubmit

--- src/main/java/in/rajat/level_4_string/day_1/DuplicatesByFreq.java
+++ src/main/java/in/rajat/level_4_string/day_1/DuplicatesByFreq.java
@@ -12,10 +12,15 @@ public class DuplicatesByFreq {
         String str = "java is fun java is powerful";
 
         Map<String, Long> result = Arrays.stream(str.split(" "))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream()
-                .filter(node-> node.getValue()>1)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        .collect(Collectors.groupingBy(
+                Function.identity(),
+                Collectors.counting()))
+        .entrySet()
+        .stream()
+        .filter(node -> node.getValue()>1)
+        .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue));
 
         System.out.println(result);
     }
