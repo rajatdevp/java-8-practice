c0a6d8a664fc369efab45a9c00a5b7e40dfad8b3
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-15 12:05:12 +0530

    level 4 day 1 problem 2

--- src/main/java/in/rajat/level_4_string/day_2/FirstNonRepeated.java
+++ src/main/java/in/rajat/level_4_string/day_2/FirstNonRepeated.java
@@ -1,6 +1,7 @@
 package in.rajat.level_4_string.day_2;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -11,9 +12,13 @@ public class FirstNonRepeated {
       String string = "java guides java concept";
 
       Optional<java.util.Map.Entry<String, Long>> result = Arrays.stream(string.split(" "))
-                      .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                              .entrySet().stream().filter(node -> node.getValue()==1)
-                      .findFirst();
+                      .collect(Collectors.groupingBy(
+                              Function.identity(),
+                              LinkedHashMap::new,
+                              Collectors.counting()))
+                              .entrySet().stream()
+              .filter(node -> node.getValue()==1)
+              .findFirst();
 
       result.ifPresent(System.out::println);
     }
