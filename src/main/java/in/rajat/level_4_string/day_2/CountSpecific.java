d09836343c0a1ce152cf03df5a0350e5eab43061
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-15 22:39:43 +0530

    level 4 day 1 problem 3

---
+++ src/main/java/in/rajat/level_4_string/day_2/CountSpecific.java
@@ -0,0 +1,16 @@
+package in.rajat.level_4_string.day_2;
+
+import java.util.Arrays;
+
+public class CountSpecific {
+
+    public static void main(String[] args){
+
+        String str = "Java is great. Java is awesome";
+
+       long result = Arrays.stream(str.split(" "))
+               .filter(s-> s.equals("Java"))
+               .count();
+       System.out.println(result);
+    }
+}
