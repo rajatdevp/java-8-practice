4a072246682644a2ed12df43032abe789a8c2290
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-18 23:38:03 +0530

    level 4 day 4 problem 2

--- src/main/java/in/rajat/level_4_string/day_4/SortByLastCharacter.java
+++ src/main/java/in/rajat/level_4_string/day_4/SortByLastCharacter.java
@@ -1,16 +1,15 @@
 package in.rajat.level_4_string.day_4;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class SortByLastCharacter {
 
     public static void main(String[] args){
 
-        List<String> words = List.of("\"apple\", \"banana\", \"cherry\", \"date\"");
+        List<String> words = List.of("apple", "banana", "cherry", "date");
 
-        System.out.println(words);
-
-       List<String> result = words.stream().sorted((a,b) -> a.charAt(a.length()-1)+"".compareTo(b.charAt(b.length()-1)+"")).toList();
+       List<String> result = words.stream().sorted(Comparator.comparing(a -> Character.toString(a.charAt(a.length() - 1)))).toList();
 
     System.out.println(result);
     }
