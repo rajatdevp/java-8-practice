e56b6207c1132dc92566d12009e3a83d9652737f
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-20 22:46:27 +0530

    level 4 day 4 problem 5

---
+++ src/main/java/in/rajat/level_4_string/day_5/MaximumVowel.java
@@ -0,0 +1,12 @@
+package in.rajat.level_4_string.day_5;
+
+import java.util.List;
+
+public class MaximumVowel {
+
+    public static void main(String[] args) {
+
+        List<String> words = List.of("education", "beautiful", "apple");
+        words.stream().sorted((s1, s2) -> {}).forEach(System.out::println);
+    }
+}
