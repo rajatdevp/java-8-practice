84e008c60a2980ebc98fe470d60b172b2c02b950
Rajat <73759265+rajatdevp@users.noreply.github.com>
2026-06-21 23:40:52 +0530

    Remove empty comparator in stream sorting

--- src/main/java/in/rajat/level_4_string/day_5/MaximumVowel.java
+++ src/main/java/in/rajat/level_4_string/day_5/MaximumVowel.java
@@ -7,7 +7,6 @@ public class MaximumVowel {
     public static void main(String[] args) {
 
         List<String> words = List.of("education", "beautiful", "apple");
-        words.stream().sorted((s1, s2) -> {}).forEach(System.out::println);
-        
+         
     }
 }
