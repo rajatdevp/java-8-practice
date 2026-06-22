6ef1ed42733f7697a60217c91b772d18beac9458
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-22 21:27:05 +0530

    level 4 day 4 problem 5

--- src/main/java/in/rajat/level_4_string/day_5/MaximumVowel.java
+++ src/main/java/in/rajat/level_4_string/day_5/MaximumVowel.java
@@ -1,12 +1,24 @@
 package in.rajat.level_4_string.day_5;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 public class MaximumVowel {
 
     public static void main(String[] args) {
 
         List<String> words = List.of("education", "beautiful", "apple");
+
+        Function<String ,Long> count = word -> word.chars()
+                .filter( ch ->"aeiou".indexOf(ch) ==-1)
+                .count();
+
+        Optional<String> result =  words.stream().max(Comparator.comparing(count));
+
+        System.out.println(result.orElse(""));
+
          
     }
 }
