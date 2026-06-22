2d5b775758fc12dfb4550a4c3efeeccb30c62114
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-22 22:07:19 +0530

    level 4 day 5 problem 2

---
+++ src/main/java/in/rajat/level_4_string/day_5/GroupByVowel.java
@@ -0,0 +1,23 @@
+package in.rajat.level_4_string.day_5;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class GroupByVowel {
+
+    public static void main(String[] args){
+
+       List<String> words = List.of("apple", "banana", "sky", "queen");
+
+       Function<String,Long> vowels = word -> word.chars()
+               .filter(ch-> "aeiou".indexOf(ch)!=-1)
+               .count();
+
+       Map<Long, List<String>> result  = words.stream()
+               .collect(Collectors.groupingBy(vowels));
+
+       System.out.println(result);
+    }
+}
