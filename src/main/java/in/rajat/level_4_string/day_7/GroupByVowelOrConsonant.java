f4475a5efeca88634b7de394d26e11cb7cecf54b
Rajat Kumar <rajatsrajesh@gmail.com>
2026-06-26 16:56:16 +0530

    level 4 day 7 problem 3

--- src/main/java/in/rajat/level_4_string/day_7/GroupByVowelOrConsonant.java
+++ src/main/java/in/rajat/level_4_string/day_7/GroupByVowelOrConsonant.java
@@ -6,12 +6,22 @@ import java.util.stream.Collectors;
 
 public class GroupByVowelOrConsonant {
 
+    static String  VOWEL = "aeiou";
     public static void main(String[] args) {
 
+
        List<String> words = List.of("apple", "banana", "egg", "orange", "sky");
+
        Map<Boolean, List<String>>  result = words.stream()
-               .collect(Collectors.partitioningBy(word-> "aeiou".indexOf(word.charAt(0))>-1));
+               .collect(Collectors.partitioningBy(word-> VOWEL.indexOf(word.charAt(0))>-1));
+
+        Map<String, List<String>> result2 = words.stream().collect(
+                Collectors.groupingBy(word ->VOWEL.indexOf(word.charAt(0))>=0
+                ?"Vowel"
+                : "Consonant"));
+       //words.stream().
 
        System.out.println(result);
+       System.out.println(result2);
     }
 }
