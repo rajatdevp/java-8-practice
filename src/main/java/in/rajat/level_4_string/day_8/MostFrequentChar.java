cdf135a9bb6167dede886014ea5a2cf55a13a6bf
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-03 23:54:50 +0530

    level 4 day 8 problem 4

--- src/main/java/in/rajat/level_4_string/day_8/MostFrequentChar.java
+++ src/main/java/in/rajat/level_4_string/day_8/MostFrequentChar.java
@@ -1,6 +1,7 @@
 package in.rajat.level_4_string.day_8;
 
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -9,15 +10,17 @@ import java.util.stream.Collectors;
 
 public class MostFrequentChar {
     public static void main(String[] args) {
-        List<String> words = List.of("apple", "banana", "cherry");
+        List<String> words = List.of("java", "guides", "streams");
 
-       Optional<Map.Entry<Character, Long>> result = words.stream()
-                .map(str-> List.of(str.toCharArray()))
-                .flatMap(List::stream)
+       Optional<Map.Entry<String, Long>> result = words.stream()
+                .map(str-> str.split(""))
+                .flatMap(Arrays::stream)
                 .collect(Collectors.groupingBy(
                         Function.identity(),
                         Collectors.counting()))
                 .entrySet()
                 .stream().max(Map.Entry.comparingByValue());
+
+       result.ifPresent(System.out::println);
     }
 }
