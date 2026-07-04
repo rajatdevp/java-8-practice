82255c642fe289fae19afdf2fb33744decaf1c54
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-04 18:06:59 +0530

    level 4 day 8 problem 6

---
+++ src/main/java/in/rajat/level_4_string/day_8/TitleCase.java
@@ -0,0 +1,9 @@
+package in.rajat.level_4_string.day_8;
+
+import java.util.List;
+
+public class TitleCase {
+    public static void main(String[] args) {
+        List<String> words = List.of("java guides", "stream api");
+    }
+}
