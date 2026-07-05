7de4adc5f2875d234dbc31e336740b80970b15e6
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-05 20:11:37 +0530

    level 4 day 8 problem 6

--- src/main/java/in/rajat/level_4_string/day_8/TitleCase.java
+++ src/main/java/in/rajat/level_4_string/day_8/TitleCase.java
@@ -1,10 +1,19 @@
 package in.rajat.level_4_string.day_8;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TitleCase {
     public static void main(String[] args) {
 
         List<String> words = List.of("java guides", "stream api");
+
+       List<String> result = words.stream().map(
+                word -> Arrays.stream(word.split(" "))
+                        .map(w-> Character.toUpperCase(w.charAt(0))+w.substring(1))
+                        .collect(Collectors.joining(" "))).toList();
+
+       System.out.println(result);
     }
 }
