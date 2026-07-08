d5c6ced6bb0a93a1e6ea8ddb0de1c55f3b0392e7
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-08 22:16:35 +0530

    level 4 day 12 problem 1 Q144 Sort strings by reverse alphabetical order

--- src/main/java/in/rajat/level_4_string/day_12/ReverseAlphabetical.java
+++ src/main/java/in/rajat/level_4_string/day_12/ReverseAlphabetical.java
@@ -12,8 +12,26 @@ public class ReverseAlphabetical {
        List<String> result =  words.stream()
                .sorted(Comparator.reverseOrder())
                .toList();
-
        System.out.println(result);
+
+       List<String> result1 = words.stream()
+               .sorted(Comparator.<String>naturalOrder().reversed())
+               .toList();
+
+       System.out.println(result1);
+
+       List<String> result3 = words.stream().
+               sorted((a,b)-> b.compareTo(a))
+               .toList();
+
+       System.out.println(result3);
+
+      List<String> result4 = words.stream()
+              .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+              .toList();
+      System.out.println(result4);
+
+
     }
 
 }
