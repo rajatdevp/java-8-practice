5f90fa4eab2e4f07081a57164c535a327e661c02
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-10 23:38:01 +0530

    level 4 day 13

---
+++ src/main/java/in/rajat/level_4_string/day_13/OneDigitOneLetter.java
@@ -0,0 +1,13 @@
+package in.rajat.level_4_string.day_13;
+
+import java.util.List;
+import java.util.regex.Matcher;
+
+public class OneDigitOneLetter {
+
+    public static void main(String[] args) {
+        List<String> list = List.of("java8", "hello", "2025");
+
+        list.stream().filter(s ->)
+    }
+}
