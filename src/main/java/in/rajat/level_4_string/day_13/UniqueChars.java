6c2f282b0cc3eb0593e337d4f7ae7543439999a4
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-11 19:52:37 +0530

    level 4 day 13 Q146 Count total unique characters across all strings

---
+++ src/main/java/in/rajat/level_4_string/day_13/UniqueChars.java
@@ -0,0 +1,28 @@
+package in.rajat.level_4_string.day_13;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class UniqueChars {
+
+    public static void main(String[] args) {
+
+        List<String> words = List.of("java", "guides");
+
+       Optional<Long> result = words.stream()
+                .map(s -> s.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting())).entrySet().stream()
+                .filter(e -> e.getValue() == 1).map(Map.Entry::getValue)
+                .reduce(Long::sum);
+
+      result.ifPresent(System.out::println);
+
+    }
+}
