ab27ebba662cc395bbab496533ff97da20d6b17f
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-11 19:19:36 +0530

    level 4 day 13 Q145 Find strings that contain at least one digit and one letter

--- src/main/java/in/rajat/level_4_string/day_13/OneDigitOneLetter.java
+++ src/main/java/in/rajat/level_4_string/day_13/OneDigitOneLetter.java
@@ -1,13 +1,18 @@
 package in.rajat.level_4_string.day_13;
 
 import java.util.List;
-import java.util.regex.Matcher;
 
 public class OneDigitOneLetter {
 
     public static void main(String[] args) {
         List<String> list = List.of("java8", "hello", "2025");
 
-        list.stream().filter(s ->)
+        List<String> validStrings = list.stream()
+                .filter(str ->
+                        str.chars().anyMatch(Character::isLetter) &&
+                        str.chars().anyMatch(Character::isDigit))
+                .toList();
+
+        System.out.println(validStrings);
     }
 }
