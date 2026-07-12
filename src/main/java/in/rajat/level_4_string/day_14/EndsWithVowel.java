fb50f810e4d54a860873d79b834d11a6f6fd0084
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-12 22:56:04 +0530

    level 4 day 14 Q148 Find the median length of strings Q149 Group strings by whether length is even or odd

--- src/main/java/in/rajat/level_4_string/day_14/EndsWithVowel.java
+++ src/main/java/in/rajat/level_4_string/day_14/EndsWithVowel.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 public class EndsWithVowel {
 
+    static String VOWELS = "aeiou";
+
     public static void main(String[] args){
 
         List<String> words =  List.of("apple", "banana", "cherry", "mango");
 
         List<String> result = words.stream().filter(
-                word-> "aioue".indexOf(word.charAt(word.length()-1)) != -1)
+                word-> VOWELS.indexOf(word.charAt(word.length()-1)) != -1)
                 .toList();
 
         System.out.println(result);
