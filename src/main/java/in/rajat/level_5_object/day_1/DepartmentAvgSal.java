0f560811c211f186878f96538a9f6036048558dd
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-15 23:25:44 +0530

    level 5 utils

---
+++ src/main/java/in/rajat/level_5_object/day_1/DepartmentAvgSal.java
@@ -0,0 +1,13 @@
+package in.rajat.level_5_object.day_1;
+
+import java.util.stream.Collectors;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
+public class DepartmentAvgSal {
+
+    public static void main(String[] args) {
+
+        EMPLOYEES.stream().collect(Collectors.groupingBy(employee -> employee. ))
+    }
+}
--- src/main/java/in/rajat/level_5_object/utils/Employee.java
+++ src/main/java/in/rajat/level_5_object/utils/Employee.java
@@ -21,6 +21,67 @@ public class Employee {
         this.experience = experience;
     }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
     public static final List<Employee> EMPLOYEES = List.of(
             new Employee(101, "Rajat", "IT", "Bangalore","Male", 85000, 29, 6),
             new Employee(102, "Priya", "HR", "Hyderabad","Female", 60000, 27, 4),
@@ -79,5 +140,6 @@ public class Employee {
             new Employee(116, "  Meena  ", " HR ", " Bangalore ", "Female", 62000, 27, 4)
 
     );
+
 
 }
