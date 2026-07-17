5cf75197910a561b9cd39e2f62248cf711a0a371
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-17 22:29:05 +0530

    level 5 day 2 Q152 : Get top 3 highest paid employees overall

--- src/main/java/in/rajat/level_5_object/utils/Employee.java
+++ src/main/java/in/rajat/level_5_object/utils/Employee.java
@@ -1,86 +1,20 @@
 package in.rajat.level_5_object.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
 import java.util.List;
 
+@ToString
+@Data
+@AllArgsConstructor
 public class Employee {
 
     int id;
     String name, department, city, gender;
     double salary;
     int age, experience;
-
-    public Employee() {
-    }
-    public Employee(int id, String name, String department, String city, String gender, double salary, int age, int experience) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-        this.gender = gender;
-        this.salary = salary;
-        this.age = age;
-        this.experience = experience;
-    }
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setExperience(int experience) {
-        this.experience = experience;
-    }
 
     public static final List<Employee> EMPLOYEES = List.of(
             new Employee(101, "Rajat", "IT", "Bangalore","Male", 85000, 29, 6),
@@ -140,20 +74,4 @@ public class Employee {
             new Employee(116, "  Meena  ", " HR ", " Bangalore ", "Female", 62000, 27, 4)
 
     );
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", department='" + department + '\'' +
-                ", city='" + city + '\'' +
-                ", gender='" + gender + '\'' +
-                ", salary=" + salary +
-                ", age=" + age +
-                ", experience=" + experience +
-                '}';
-    }
-
-
 }
