c082cc2f81ef7428b8a3ee00cea7fb70b2df618d
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-22 23:40:34 +0530

    level 5 day 3 Q159 : Find second highest salary in each department init

---
+++ src/main/java/in/rajat/level_5_object/day_3/DepartmentSecondHighest.java
@@ -0,0 +1,10 @@
+package in.rajat.level_5_object.day_3;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
+public class DepartmentSecondHighest {
+
+        public static  void main(String[] args) {
+          EMPLOYEES
+        }
+}
