08c81052e93a89aca64b142969053e6aff1fc23a
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-23 23:59:44 +0530

    level 5 day 3 Q160 : Convert List<Employee> → Map<id, name> (handle duplicate keys) init

--- src/main/java/in/rajat/level_5_object/day_3/MapListToMap.java
+++ src/main/java/in/rajat/level_5_object/day_3/MapListToMap.java
@@ -2,7 +2,8 @@ package in.rajat.level_5_object.day_3;
 
 import in.rajat.level_5_object.utils.Employee;
 
-import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
@@ -11,6 +12,21 @@ public class MapListToMap {
 
     public static void main(String[] args) {
 
-        EMPLOYEES.stream().collect(Collectors.groupingBy(Employee::getId))
+       Map<Integer, String> result =  EMPLOYEES.stream()
+               .collect(Collectors.toMap(
+                       Employee::getId,
+                       Employee::getName, //just Exception in thread "main" java.lang.IllegalStateException: Duplicate key 101
+                       (existing, duplicate) -> duplicate));//for last duplicate
+                       //
+
+       System.out.println(result);
+
+        Map<Integer, List<String>> result2 = EMPLOYEES.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getId,
+                        Collectors.mapping(Employee::getName, Collectors.toList())
+                ));//if result need to be list of all names
+
+        System.out.println(result2);
     }
 }
