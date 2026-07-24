21e9ddbaf9ca393beda952dc762a8e6897edc2cc
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-24 23:03:39 +0530

    level 5 day 4 Q161 : Find youngest employee

--- src/main/java/in/rajat/level_5_object/day_4/OldestEmp.java
+++ src/main/java/in/rajat/level_5_object/day_4/OldestEmp.java
@@ -3,6 +3,9 @@ package in.rajat.level_5_object.day_4;
 import in.rajat.level_5_object.utils.Employee;
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
 
@@ -15,6 +18,11 @@ public class OldestEmp {
                .orElse(null);
 
        System.out.println(result);
+
+      Map<String, Optional<Employee>>  result1 = EMPLOYEES.stream()
+              .collect(Collectors.groupingBy(Employee::getDepartment,
+                      Collectors.maxBy(Comparator.comparingInt(Employee::getAge))));
+      System.out.println(result1);
 
 
     }
