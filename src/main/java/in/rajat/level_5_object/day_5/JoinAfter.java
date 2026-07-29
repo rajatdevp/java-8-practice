e60ca1546f67055d1042a48c7c6099d344d26fed
Rajat Kumar <rajatsrajesh@gmail.com>
2026-07-29 23:04:34 +0530

    level 5 day 5 Q165 Find employees joined after 2020

---
+++ src/main/java/in/rajat/level_5_object/day_5/JoinAfter.java
@@ -0,0 +1,19 @@
+package in.rajat.level_5_object.day_5;
+
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+public class JoinAfter {
+
+    public static void main(String[] args) {
+
+       List<Employee>  result = EMPLOYEES.stream()
+               .filter(emp-> emp.getJoiningDate().getYear() > 2020)
+               .toList();
+
+       System.out.println(result);
+       System.out.println(result.size());
+    }
+}
