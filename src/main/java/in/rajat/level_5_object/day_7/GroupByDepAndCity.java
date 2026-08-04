efb134d89fb3eeba792dbcb48d238f0533093793
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-04 23:10:44 +0530

    level 5 day 7 Q175 Multi-field grouping (dept + city)

---
+++ src/main/java/in/rajat/level_5_object/day_7/GroupByDepAndCity.java
@@ -0,0 +1,23 @@
+package in.rajat.level_5_object.day_7;
+
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
+public class GroupByDepAndCity {
+
+    public static void main(String[] args){
+
+      Map<String, Map<String, Long>> result =  EMPLOYEES.stream().
+              collect(Collectors.groupingBy(
+                      Employee::getDepartment,
+                      Collectors.groupingBy(
+                              Employee::getCity,
+                              Collectors.counting())));
+
+      System.out.println(result);
+    }
+}
