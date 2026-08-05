90c2a449604c552cc0f1b1338d30c8ed5c6a4bf6
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-05 23:34:28 +0530

    level 5 day 7 Q176 Find employees with salary > average salary

--- src/main/java/in/rajat/level_5_object/day_8/FindByAvgSal.java
+++ src/main/java/in/rajat/level_5_object/day_8/FindByAvgSal.java
@@ -1,8 +1,19 @@
 package in.rajat.level_5_object.day_8;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class FindByAvgSal {
 
     public static void main(String[] args) {
+
+        double avg = EMPLOYEES.stream().mapToDouble(Employee::getSalary).average().orElseThrow();
+        List<Employee> result = EMPLOYEES.stream().filter(emp-> emp.getSalary()> avg ).toList();
+
+        System.out.println(result);
 
     }
 }
