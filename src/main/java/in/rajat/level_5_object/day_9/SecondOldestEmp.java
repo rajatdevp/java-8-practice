7ca2fbd40767a56a89fb04cc5a4d4db53d91d474
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-11 23:49:33 +0530

    level 5 day 9 Q181 Find second-oldest employee in each department

--- src/main/java/in/rajat/level_5_object/day_9/SecondOldestEmp.java
+++ src/main/java/in/rajat/level_5_object/day_9/SecondOldestEmp.java
@@ -1,8 +1,28 @@
 package in.rajat.level_5_object.day_9;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.Comparator;
+import java.util.Optional;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class SecondOldestEmp {
 
     public static void main(String[] args) {
 
+       Optional<Employee> result = EMPLOYEES.stream()
+               .sorted(Comparator.comparing(Employee::getJoiningDate).reversed())
+               .skip(1)
+               .findFirst();
+
+       result.ifPresent(System.out::println);
+
+        Optional<Employee> result2 = EMPLOYEES.stream()
+                .sorted(Comparator.comparingInt(Employee::getAge).reversed())
+                .skip(1)
+                .findFirst();
+
+        result2.ifPresent(System.out::println);
     }
 }
