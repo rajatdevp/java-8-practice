8927dc2b9a8e41d5e5868da9dcd7d0f90e334b52
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-11 23:41:32 +0530

    level 5 day 9 Q180 Find employees whose salary is between 80k-100k

--- src/main/java/in/rajat/level_5_object/day_9/SalaryBetween.java
+++ src/main/java/in/rajat/level_5_object/day_9/SalaryBetween.java
@@ -1,9 +1,21 @@
 package in.rajat.level_5_object.day_9;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class SalaryBetween {
 
     public static void main(String[] args){
 
+       List<Employee> result = EMPLOYEES.stream().filter(
+                emp ->  emp.getSalary() > 80000 &&
+                        emp.getSalary() < 100000 )
+                .toList();
+
+       System.out.println(result);
     }
 
 }
