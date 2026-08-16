14d80af4bea6425a1113b95287c929e35d7729c8
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-17 04:51:04 +0530

    level 5 day 11 Q187 Employees who joined before 2020 and salary > 90k

--- src/main/java/in/rajat/level_5_object/day_11/FilterByJoiningAndSal.java
+++ src/main/java/in/rajat/level_5_object/day_11/FilterByJoiningAndSal.java
@@ -1,9 +1,22 @@
 package in.rajat.level_5_object.day_11;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class FilterByJoiningAndSal {
 
     public static void main(String[] args) {
 
+       List<Employee> result = EMPLOYEES.stream()
+               .filter(emp->
+                       (emp.getSalary()>90000
+                               && emp.getJoiningDate()
+                               .getYear()<2020))// better emp.isBefore(LocalDate.of(2020,1,1));
+               .toList();
 
+       for (Employee employee : result) System.out.println(employee);
     }
 }
