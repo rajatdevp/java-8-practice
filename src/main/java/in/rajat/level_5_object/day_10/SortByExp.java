c600079620c32c02770d765358f9b39140af5d67
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-16 21:49:42 +0530

    level 5 day 10 Q185 Sort employees by experience descending

---
+++ src/main/java/in/rajat/level_5_object/day_10/SortByExp.java
@@ -0,0 +1,24 @@
+package in.rajat.level_5_object.day_10;
+
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
+public class SortByExp {
+
+    public static void main(String[] args) {
+
+       List<Employee> result = EMPLOYEES.stream().sorted(
+                Comparator.comparingInt(Employee::getExperience)
+                        .reversed())
+                .toList();
+
+       for (Employee employee : result) {
+           System.out.println(employee);
+       }
+
+    }
+}
