848bd40fafa1e73b6d70e2444ec9f6fd4cee7eec
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-29 23:52:30 +0530

    level 5 day 11 Q189 Find the employee with highest salary-to-experience ratio init

---
+++ src/main/java/in/rajat/level_5_object/day_11/HighestSalaryToExperienceRatio.java
@@ -0,0 +1,10 @@
+package in.rajat.level_5_object.day_11;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
+public class HighestSalaryToExperienceRatio {
+
+    public static void main(String[] args) {
+        EMPLOYEES.stream().collect()
+    }
+}
