daa7a7eac0e2b54e859d7639fa4549b506304f49
Rajat Kumar <rajatsrajesh@gmail.com>
2026-09-09 21:11:45 +0530

    level 5 day 14 Q193 Employees with salary > 1 lakh

--- src/main/java/in/rajat/level_5_object/day_13/SalGreater.java
+++ src/main/java/in/rajat/level_5_object/day_13/SalGreater.java
@@ -1,8 +1,19 @@
 package in.rajat.level_5_object.day_13;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class SalGreater {
 
     public static void main(String[] args) {
 
+       List<Employee> result = EMPLOYEES.stream()
+                .filter(emp-> emp.getSalary() > 100000)
+                .toList();
+
+       System.out.println(result);
     }
 }
