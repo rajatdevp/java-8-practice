dc88cf3422db28230c81419e14417b817c6222b3
Rajat Kumar <rajatsrajesh@gmail.com>
2026-09-09 21:23:53 +0530

    level 5 day 14 Q194 Group by department and sort employees by salary inside each group

--- src/main/java/in/rajat/level_5_object/day_13/GroupAndSort.java
+++ src/main/java/in/rajat/level_5_object/day_13/GroupAndSort.java
@@ -1,7 +1,23 @@
 package in.rajat.level_5_object.day_13;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class GroupAndSort {
     public static void main(String[] args) {
         // Grouping and sorting logic will go here
+    Map<String, List<Employee>> result = EMPLOYEES.stream()
+            .collect(Collectors.groupingBy(
+                    Employee::getDepartment,
+                    Collectors.collectingAndThen(Collectors.toList(), list -> list.stream()
+                            .sorted(Comparator.comparing(Employee::getSalary).reversed()).toList())));
+
+    System.out.println(result);
     }
 }
