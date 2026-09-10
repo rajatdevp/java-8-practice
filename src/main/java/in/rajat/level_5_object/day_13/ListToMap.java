62e3863fb160168025e29dfd2b0f84702080ccc4
Rajat Kumar <rajatsrajesh@gmail.com>
2026-09-10 23:38:09 +0530

    level 5 day 14 Q199 Convert List<Employee> to Map<dept, totalSalary>

--- src/main/java/in/rajat/level_5_object/day_13/ListToMap.java
+++ src/main/java/in/rajat/level_5_object/day_13/ListToMap.java
@@ -1,8 +1,21 @@
 package in.rajat.level_5_object.day_13;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class ListToMap {
 
     public static void main(String[] args) {
 
+        Map<String, Double> result = EMPLOYEES.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.summingDouble(Employee::getSalary)));
+
+        System.out.println(result);
     }
 }
