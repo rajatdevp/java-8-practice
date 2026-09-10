dad87bdab5c616432b58b543cb1c9875fed2a758
Rajat Kumar <rajatsrajesh@gmail.com>
2026-09-10 23:33:38 +0530

    level 5 day 14 Q199 Employees whose gender is Female and city is Bengaluru

--- src/main/java/in/rajat/level_5_object/day_13/MultiFilter.java
+++ src/main/java/in/rajat/level_5_object/day_13/MultiFilter.java
@@ -1,7 +1,20 @@
 package in.rajat.level_5_object.day_13;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.List;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
 public class MultiFilter {
 
     public static void main(String[] args) {
+
+        List<Employee> result =EMPLOYEES.stream()
+                .filter(employee -> employee.getGender().equals("Male"))
+                .filter(employee -> employee.getCity().equals("Bangalore"))
+                .toList();
+
+        System.out.println(result);
     }
 }
