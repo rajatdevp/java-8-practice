57ecb41bfeac01af44c14eb3b5444626e4163fd6
Rajat Kumar <rajatsrajesh@gmail.com>
2026-09-10 23:21:06 +0530

    level 5 day 14 Q198 Find top 2 experienced employees overall init

---
+++ src/main/java/in/rajat/level_5_object/day_13/TopExperienced.java
@@ -0,0 +1,11 @@
+package in.rajat.level_5_object.day_13;
+
+import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
+
+public class TopExperienced {
+
+    public static void main(String[] args) {
+
+        EMPLOYEES
+    }
+}
