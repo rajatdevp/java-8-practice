0b9935d3586cc3ec3060f600c509dfe8fe6225b9
Rajat Kumar <rajatsrajesh@gmail.com>
2026-09-10 23:23:40 +0530

    level 5 day 14 Q198 Find top 2 experienced employees overall

--- src/main/java/in/rajat/level_5_object/day_13/TopExperienced.java
+++ src/main/java/in/rajat/level_5_object/day_13/TopExperienced.java
@@ -1,11 +1,21 @@
 package in.rajat.level_5_object.day_13;
 
+import in.rajat.level_5_object.utils.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+
 import static in.rajat.level_5_object.utils.Employee.EMPLOYEES;
 
 public class TopExperienced {
 
     public static void main(String[] args) {
 
-        EMPLOYEES
+        List<Employee> result = EMPLOYEES.stream()
+                .sorted(Comparator.comparingInt(Employee::getExperience).reversed())
+                .limit(2).toList();
+
+        System.out.println(result);
+
     }
 }
